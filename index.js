385c78891c14b6c892c744709493c90ac2abcae6
Sinchana-Rai <sinchana.srai@gmail.com>
2024-11-13 19:52:34 -0500

    added form and res.download

--- index.js
+++ index.js
@@ -1,73 +1,17 @@
-// const express = require("express")
-
-// const app = express()
-// const port = 3000
-
-
-// // serve static files from the styles directory
-// app.use(express.static("./styles"));
-
-// // require the filesystem module
-// const fs = require("fs");
-// // define the template engine
-// app.engine("gettingStarted", (filePath, options, callback) => {
-//   fs.readFile(filePath, (err, content) => {
-//     if (err) return callback(err);
-
-//     // Here, we take the content of the template file,
-//     // convert it to a string, and replace sections of
-//     // it with the values being passed to the engine.
-//     const rendered = content
-//       .toString()
-//       .replaceAll("#title#", `${options.title}`)
-//       .replace("#content#", `${options.content}`);
-//     return callback(null, rendered);
-//   });
-// });
-
-// app.set("views", "./views"); // specify the views directory
-// app.set("view engine", "gettingStarted"); // register the template engine
-
-// app.get("/", (req, res) => {
-//   const options = {
-//     title: "Getting Started",
-//     content:
-//       "Overview</code> \
-//       The general rendering process of Pug is simple\
-//       <code>pug.compile()</code>  will compile the Pug source code into a JavaScript \
-//       function that takes a data object (called “ <code> locals </code>\
-//       as an argument.<br><br> \
-//       Call that resultant function with your data, and voilà!, it will return a string of HTML rendered with your data. \
-//       The compiled function can be re-used, and called with different sets of data.<br>\
-//       For a look at some popular view engines, check out the documentation for \
-//       <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
-//       <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
-//       <a href='https://www.npmjs.com/package/ejs'>EJS</a>. \
-//       More complete front-end libraries like React, Angular, and Vue \
-//       also have Express integrations.",
-    
-//   };
-
-//   res.render("index", options);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server listening on port: ${port}.`);
-// });
-
-// ===================================================
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 
 // serve static files from the styles directory
 app.use(express.static("./styles"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // require the filesystem module
 const fs = require("fs");
-// define the template engine
+// defining the template engine
 app.engine("gettingStarted", (filePath, options, callback) => {
   fs.readFile(filePath, (err, content) => {
     if (err) return callback(err);
@@ -83,10 +27,10 @@ app.engine("gettingStarted", (filePath, options, callback) => {
   });
 });
 
-app.set("views", "./views"); // specify the views directory
-app.set("view engine", "gettingStarted"); // register the template engine
+//app.set("views", "./views"); // specify the views directory
+app.set("view engine", "gettingStarted"); 
 
-app.get("/", (req, res) => {
+app.get("/gettingStarted", (req, res) => {
   const options = {
     title: "Getting Started",
     content:
@@ -95,12 +39,12 @@ app.get("/", (req, res) => {
       render this page using custom content within the template.<br><br>",
   };
 
-  res.render("index", options);
+  res.render("index.gettingStarted", { ...options, engine: "gettingStarted" });
 });
 
 //=======================================================
 
-app.engine("ExpressIntegration", (filePath, options, callback) => {
+  app.engine("ExpressIntegration", (filePath, options, callback) => {
     fs.readFile(filePath, (err, content) => {
       if (err) return callback(err);
   
@@ -115,8 +59,8 @@ app.engine("ExpressIntegration", (filePath, options, callback) => {
     });
   });
   
-  app.set("views", "./views"); // specify the views directory
-  app.set("view engine", "ExpressIntegration"); // register the template engine
+  //app.set("views", "./views"); // specify the views directory
+  //app.set("view engine", "ExpressIntegration"); // register the template engine
   
   app.get("/ExpressIntegration", (req, res) => {
     const options = {
@@ -125,10 +69,29 @@ app.engine("ExpressIntegration", (filePath, options, callback) => {
         "This is the Express Integration page<br>",
     };
   
-    res.render("index2", options);
-  });
+    res.render("index2.ExpressIntegration", { ...options, engine: "ExpressIntegration" });
+  })
 
-  //==============================================================
+  app.post('/submit-data', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    
+    // Process the form data (e.g., save to a database)
+    console.log('Received data:', { name, email });
+    
+    // Send a response
+    res.send('Form submitted successfully!');
+});
+
+app.get('/download-image', (req, res) => {
+    const filePath ='./styles/photo.jpg'; 
+    res.download(filePath, 'sample.jpg', (err) => {
+    });
+  });
+  
+
+  //=======================================================
+
   app.engine("reference", (filePath, options, callback) => {
     fs.readFile(filePath, (err, content) => {
       if (err) return callback(err);
@@ -145,22 +108,21 @@ app.engine("ExpressIntegration", (filePath, options, callback) => {
   });
   
   app.set("views", "./views"); // specify the views directory
-  app.set("view engine", "reference"); // register the template engine
+  //app.set("view engine", "reference"); // register the template engine
   
   app.get("/reference", (req, res) => {
     const options = {
-      title: "API Reference",
+      title: "API References",
       content:
-        "This is the API references page<br>",
+        "This is the API References page<br>",
     };
   
-    res.render("index3", options);
+    res.render("index3.reference", { ...options, engine: "reference" });
   });
 
+  
   //==============================================================
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}.`);
 });
-
-
